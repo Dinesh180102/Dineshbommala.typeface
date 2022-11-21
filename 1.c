b7f895934d7149e0b729b8462ce2c1741266dfe8
Dinesh180102 <96431404+Dinesh180102@users.noreply.github.com>
2022-11-21 15:56:29 +0530

    Update and rename 1.py to 1.c

---
+++ 1.c
@@ -0,0 +1,16 @@
+# Dineshbommala.typeface
+#include <stdio.h>
+#include <math.h>
+int main(){
+  int h;
+  scanf("%d",h);
+  int o;
+  int i=0;
+  while(h>0){
+    int z=u%3;
+    o=o+(z*(int(pow(10,i)));
+    u=u/3;
+    i=i+1;}
+  printf("%d",o);
+         }
+
